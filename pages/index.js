52dedfd1ac02fe47e8d62c0abb413d252a6fd776
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-07 02:26:32 -0300

    Implementing CSS

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
+import React from 'react';
 import Link from "../src/components/Link";
 
+function Title({ children, as }) {
+    const Tag = as;
+    return (
+        <React.Fragment>
+            <Tag>
+                {children}
+            </Tag>
+            <style jsx>{`          
+                ${Tag} {
+                    color: red;
+                }
+            `}</style>
+        </React.Fragment>
+    );
+}
+
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Alura Cases - Home Page</h1>
-      <Link href="/faq">Ir para a página do FAQ</Link>
-    </div>
-  );
+    return (
+        <div>
+            <Title as="h1">Alura Cases - Home Page</Title>
+            <Link href="/faq">Ir para a página do FAQ</Link>
+        </div>
+    );
 }
